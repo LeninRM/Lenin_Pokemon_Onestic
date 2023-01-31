3faf48373daf63de4b4253dcd7b5d09b2da2f623
Lenin <94408917+NinxCode@users.noreply.github.com>
2023-01-31 11:20:06 +0100

    created shard service, added new func to dataservice for detail, detail modal created and working

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ResponseList } from '../interfaces/response-list';
-import { Result } from '../interfaces/response-list';
-
+import { ResponsePokemon } from '../interfaces/response-pokemon';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,8 +11,14 @@ export class DataService {
   constructor(public http: HttpClient) { };
 
   public urlList: string = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';/* ?limit=100000&offset=0 */
+  public urlPokemon: string  = ' https://pokeapi.co/api/v2/pokemon/';
 
   public getList(): Observable<ResponseList>{
     return this.http.get<ResponseList>(this.urlList)
   }
+
+  public getPokemon(name: string): Observable<ResponsePokemon>{
+    return this.http.get<ResponsePokemon>(this.urlPokemon + name)
+  }
 }
+
--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, EventEmitter } from '@angular/core';
 import { ResponseList } from 'src/app/interfaces/response-list';
 import { DataService } from 'src/app/services/data.service';
 import { SharedService } from 'src/app/services/shared.service';
-import { MatPaginatorIntl  } from '@angular/material/paginator';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { ResponsePokemon } from 'src/app/interfaces/response-pokemon';
 
 @Component({
   selector: 'app-list',
@@ -12,8 +13,9 @@ import { MatPaginatorIntl  } from '@angular/material/paginator';
 export class ListComponent {
   public constructor(public service: DataService, public shared: SharedService, public paginator: MatPaginatorIntl) { }
   public list: ResponseList | undefined;
+  public pokemon: ResponsePokemon | undefined;
   public urlImg: string = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
-  public layout: string = "table";
+  public layout: string = "grid";
 
   /* Paginator */
   public totalItems: number = 0;
@@ -22,10 +24,9 @@ export class ListComponent {
   public minIndex: number = 0;
 
   public ngOnInit(): void {
-    this.paginator.itemsPerPageLabel="Pokémon per page";
+    this.paginator.itemsPerPageLabel = "Pokémon per page";
     this.getLayout();
     this.getResponseList();
-    console.log(this.getIdFromUrl("https://pokeapi.co/api/v2/pokemon/1/"));
   }
 
   public getLayout(): void {
@@ -38,7 +39,14 @@ export class ListComponent {
     this.service.getList().subscribe(response => {
       this.list = response;
       this.totalItems = this.list.count
-      console.log(this.list);
+/*       console.log(this.list);
+ */    })
+  }
+
+  public getResponsePokemon(id: string): void {
+    this.service.getPokemon(id).subscribe(response => {
+      this.pokemon = response;
+      console.log(this.pokemon);
     })
   }
 
@@ -47,10 +55,10 @@ export class ListComponent {
     return urlParts[urlParts.length - 2];
   }
 
-public onPageChange($event: any){
-  console.log($event)
-  this.minIndex = $event.pageIndex * $event.pageSize;
-  this.maxIndex = this.minIndex + $event.pageSize;
-}
+  public onPageChange($event: any) {
+    console.log($event)
+    this.minIndex = $event.pageIndex * $event.pageSize;
+    this.maxIndex = this.minIndex + $event.pageSize;
+  }
 
 }
